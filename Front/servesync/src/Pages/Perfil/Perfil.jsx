e39e8d36a3f957d95bb9143bf7e132d79bcaf7dc
Felipe Engels <engelsope@gmail.com>
2025-06-19 19:46:54 -0400

    Header ajuste

--- Front/servesync/src/Pages/Perfil/Perfil.jsx
+++ Front/servesync/src/Pages/Perfil/Perfil.jsx
@@ -1,22 +1,25 @@
 import Header from '../../Components/Header/Header';
 import './Perfil.css'; 
+
 const profilePage = () => {
-    return(
+    return (
         <div>
-            <Header/>
-            <div className='container'>
-                <div className='profile-card'>
-                    <img src="/user.png" alt="User" className='profile-image' />
-                    <h2 className='profile-name'>Nombre de Usuario</h2>
-                    <h2 className='profile-email'>Email</h2>
+            <Header />
+            <div className='main-content'>
+                <div className='container'>
+                    <div className='profile-card'>
+                        <img src="/user.png" alt="User" className='profile-image' />
+                        <h2 className='profile-name'>Nombre de Usuario</h2>
+                        <h2 className='profile-email'>Email</h2>
+                    </div>
                 </div>
 
+                <div className='containerRight'>
+                    <h2>ajkshdajksdhkjasd</h2>
+                </div>
             </div>
         </div>
-        
-
     );
-
-}
+};
 
 export default profilePage;
