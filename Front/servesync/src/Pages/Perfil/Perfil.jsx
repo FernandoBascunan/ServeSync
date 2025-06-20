3a8fd8dc62de059140c5675bde0699b06aaf6df1
Felipe Engels <engelsope@gmail.com>
2025-06-20 03:43:01 -0400

    Ajuste Home

--- Front/servesync/src/Pages/Perfil/Perfil.jsx
+++ Front/servesync/src/Pages/Perfil/Perfil.jsx
@@ -2,6 +2,9 @@ import Header from '../../Components/Header/Header';
 import './Perfil.css'; 
 
 const profilePage = () => {
+    const handleLogin = () => {
+    window.location.href = "/Login"; // o tu ruta deseada
+    };
     return (
         <div>
             <Header />
@@ -9,9 +12,17 @@ const profilePage = () => {
                 <div className='container'>
                     <div className='profile-card'>
                         <img src="/user.png" alt="User" className='profile-image' />
-                        <h2 className='profile-name'>Nombre de Usuario</h2>
+                        <h2 className='profile-name'>Nombre de la PYME</h2>
+                        <h2 className='profile-email'>Rut de la PYME</h2>
+                        <h2 className='profile-name'>Nombre Dueno</h2>
                         <h2 className='profile-email'>Email</h2>
+                        <h2 className='profile-name'>Telefono</h2>
                     </div>
+                    <div>
+                        <button className='login-button'>Editar Perfil</button>
+                        <button className='login-button' onClick={handleLogin}>Cerrar Sesion</button>
+                    </div>
+
                 </div>
 
                 <div className='containerRight'>
